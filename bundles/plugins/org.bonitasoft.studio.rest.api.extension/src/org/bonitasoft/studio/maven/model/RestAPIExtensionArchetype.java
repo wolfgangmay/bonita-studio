f95d2e482f6d326fb2889cf104710a8a8a566e52
Romain Bioteau <romain.bioteau@bonitasoft.com>
2024-09-27 17:30:22 +0200

    fix(restApi): update archetype (#3640)
    Closes STUDIO-4542

--- bundles/plugins/org.bonitasoft.studio.rest.api.extension/src/org/bonitasoft/studio/maven/model/RestAPIExtensionArchetype.java
+++ bundles/plugins/org.bonitasoft.studio.rest.api.extension/src/org/bonitasoft/studio/maven/model/RestAPIExtensionArchetype.java
@@ -14,7 +14,7 @@ public class RestAPIExtensionArchetype {
 
     public static final String ARTIFACT_ID = "bonita-rest-api-extension-archetype";
     public static final String GROUP_ID = "org.bonitasoft.archetypes";
-    public static final String VERSION = "1.7.1";
+    public static final String VERSION = "1.7.2";
     public static final IArchetype INSTANCE = new Archetype(GROUP_ID, ARTIFACT_ID, VERSION);
    
 
